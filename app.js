1e7fbbfdcb2d4432bdb485fb78578ab8b55feeed
dhuu2003 <101379737+dhuu2003@users.noreply.github.com>
2022-07-17 06:39:29 +0700

    version 1.0

--- app.js
+++ app.js
@@ -1,51 +1,60 @@
-var search = document.querySelector('.search')
-var city = document.querySelector('.city')
-var country = document.querySelector('.country')
-var value = document.querySelector('.value')
-var shortDesc = document.querySelector('.short-desc')
-var visibility = document.querySelector('.visibility span')
-var wind = document.querySelector('.wind span')
-var sun = document.querySelector('.sun span')
-var value = document.querySelector('.value')
-var time = document.querySelector('.time');
-var content = document.querySelector('.content');
-var body = document.querySelector('body');
-async function changeWeatherUI(capitalValue) {
+var input = document.querySelector('input');
+var button = document.querySelector('button');
+var form = document.querySelector('form');
+var todos = document.querySelector('.todos')
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let val = input.value.trim();
+    if (val) {
+        addTodoElement({
+            text: val,
 
-    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${capitalValue}&appid=7ed236f060b69f0b2f8f7755f264d9db`
+        })
+        saveTodoList();
 
-    let data = await fetch(apiURL).then(res => res.json())
-    console.log(data);
-    if (data.cod === 200) {
-        city.innerText = data.name
-        country.innerText = data.sys.country;
-        visibility.innerText = data.visibility + 'm';
-        wind.innerText = data.wind.speed + 'm/s';
-        sun.innerText = data.main.humidity + '%';
-        let temp = Math.round(data.main.temp - 273);
-        value.innerText = temp + ''
-        shortDesc.innerText = data.weather[0] ? data.weather[0].main : ' ';
-        time.innerText = new Date().toLocaleDateString('vi');
-        body.setAttribute('class', 'hot')
-        if (temp <= 25) {
-            body.setAttribute('class', 'cold');
-        }
-        if (temp <= 22) {
-            body.setAttribute('class', 'hot');
-        }
-    } else {
-        content.classList.add('hide');
+
     }
-
-
-
-}
-
-search.addEventListener('keypress', function (e) {
-    if (e.code === 'Enter') {
-        let capitalValue = search.value.trim();
-        changeWeatherUI(capitalValue);
-    }
-
+    input.value = '';
 })
-changeWeatherUI('Ha Noi')
+function addTodoElement(todo) {
+    //     <li>
+    //     <span>Test</span>
+    //     <i class="far fa-trash-alt"></i>
+    // </li>
+    var li = document.createElement('li');
+    li.innerHTML = ` <span>${todo.text}</span>
+    <i class="far fa-trash-alt"></i>
+ </li>`
+    if (todo.status === 'completed') {
+        li.setAttribute('class', 'completed');
+    }
+    li.addEventListener('click', function () {
+        this.classList.toggle('completed')
+        saveTodoList()
+    })
+    li.querySelector('i').addEventListener('click', function () {
+        this.parentElement.remove();
+        saveTodoList()
+    })
+    todos.appendChild(li);
+}
+function saveTodoList() {
+    let todoList = document.querySelectorAll('li');
+    let todoStorage = [];
+    todoList.forEach(function (item) {
+        let text = item.querySelector('span').innerText;
+        let status = item.getAttribute('class');
+        todoStorage.push({
+            text,
+            status
+        })
+    })
+    localStorage.setItem('todoList', JSON.stringify(todoStorage));
+}
+function init() {
+    let data = JSON.parse(localStorage.getItem('todoList'));
+    data.forEach(function (item) {
+        addTodoElement(item)
+    })
+}
+init()
